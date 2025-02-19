2606972d4c3e1f75dc2966a92816797e6d76eb37
Abdur Rahaman Rahi <109844584+rahiInstant@users.noreply.github.com>
2025-02-19 18:26:12 +0600

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,11 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
     darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+	content: [
+		"./src/**/*.{js,ts,jsx,tsx,mdx}"
+	  ],
   theme: {
   	extend: {
   		colors: {
